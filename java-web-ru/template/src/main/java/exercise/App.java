d5b8fdcb68303630c899063aa84dbeee027d3fd5
@hexlet/cli <support@hexlet.io>
2025-04-28 01:19:29 +0300

    submit java-web-ru/template

--- java-web-ru/template/src/main/java/exercise/App.java
+++ java-web-ru/template/src/main/java/exercise/App.java
@@ -11,32 +11,29 @@ import io.javalin.rendering.template.JavalinJte;
 
 public final class App {
 
-    // Каждый пользователь представлен объектом класса User
     private static final List<User> USERS = Data.getUsers();
 
     public static Javalin getApp() {
-
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
             config.fileRenderer(new JavalinJte());
         });
 
-        // BEGIN
         app.get("/users", ctx -> {
             var header = "Список пользователей";
-            var page = new UsersPage(users, header);
+            var page = new UsersPage(USERS, header);
             ctx.render("users/index.jte", model("page", page));
         });
 
         app.get("/users/{id}", ctx -> {
-            var id = ctx.getParam("id");
+            var id = Long.parseLong(ctx.pathParam("id"));
             var user = USERS.stream()
-                    .filter(c -> c.get("id").equals(id))
+                    .filter(u -> u.getId() == id)
                     .findFirst()
                     .orElseThrow(() -> new NotFoundResponse("User not found"));
-            ctx.render("users/show.jte", model("user", user));
-        })
-        // END
+            var page = new UserPage(user);
+            ctx.render("users/show.jte", model("page", page));
+        });
 
         app.get("/", ctx -> {
             ctx.render("index.jte");
